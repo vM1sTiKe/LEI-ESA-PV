5421d85564eab03cf34ca2b0718b5c8ba54b1724
Bruno Daniel <48827010+vM1sTiKe@users.noreply.github.com>
2024-12-22 23:43:05 +0000

    trying to do auth2.0

---
+++ AeroBites/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿namespace AeroBites.Models
+{
+    public class Account
+    {
+        //vai ter a chave primaria e um google id
+
+        //https://developers.google.com/identity/gsi/web/guides/verify-google-id-token
+
+        //https://developers.google.com/identity/gsi/web/reference/html-reference
+
+//https://www.youtube.com/watch?v=uCpOLZiSb3s
+    }
+}

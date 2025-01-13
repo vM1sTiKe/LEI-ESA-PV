36d2db09bb63f605e67b4f659327c38f7504170f
Bruno Daniel <48827010+vM1sTiKe@users.noreply.github.com>
2025-01-13 21:10:03 +0000

    .

--- AeroBites/Models/Account.cs
+++ AeroBites/Models/Account.cs
@@ -2,6 +2,15 @@
 {
     public class Account
     {
+        //https://console.cloud.google.com/apis/dashboard?inv=1&invt=AbmrFA&project=esa-pv
+        public int Id { get; set; }
+
+        public int GoogleId { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+
+
         //vai ter a chave primaria e um google id
 
         //https://developers.google.com/identity/gsi/web/guides/verify-google-id-token

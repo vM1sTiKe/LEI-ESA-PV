a8dd628cd21a25a53409ab429dff1274645948cd
Bruno Daniel <48827010+vM1sTiKe@users.noreply.github.com>
2025-01-30 18:50:43 +0000

    Enums

---
+++ AeroBites/Enums.cs
@@ -0,0 +1,22 @@
+﻿namespace AeroBites
+{
+    public static class Enums
+    {
+        public enum RestaurantStatus
+        {
+            WaitingAcceptance,
+            Valid,
+            Rejected
+        }
+
+        public enum OrderStatus
+        {
+            Choosing,
+            Placed,
+            Preparing,
+            OnTheWay,
+            Waiting,
+            Recieved
+        }
+    }
+}

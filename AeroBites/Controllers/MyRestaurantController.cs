c10767726ed8897fa3c0ec930f7c05ed6d1a6e6c
Bruno Daniel <48827010+vM1sTiKe@users.noreply.github.com>
2025-02-13 19:23:04 +0000

    Rename controller

---
+++ AeroBites/Controllers/MyRestaurantController.cs
@@ -0,0 +1,20 @@
+﻿using AeroBites.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AeroBites.Controllers
+{
+    [Authorize]
+    public class MyRestaurantController : Controller
+    {
+        private readonly AeroBitesContext _context;
+
+        public MyRestaurantController(AeroBitesContext context) {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

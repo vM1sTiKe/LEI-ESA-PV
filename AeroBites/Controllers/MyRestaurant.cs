339cff6d1298bf5d81aff6fdf7c95377b68246dd
Bruno Daniel <48827010+vM1sTiKe@users.noreply.github.com>
2025-02-13 19:20:41 +0000

    MYRestaurant controller
    Created new controller to hold the views related to "my restaurant"

---
+++ AeroBites/Controllers/MyRestaurant.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AeroBites.Controllers
+{
+    public class MyRestaurant : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

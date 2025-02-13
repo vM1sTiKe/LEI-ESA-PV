404cceb841cb3266192baa77627304309f51ba6e
Bruno Daniel <48827010+vM1sTiKe@users.noreply.github.com>
2025-02-13 19:20:13 +0000

    User extension IsAdmin
    Added a new user extension to be easy to know if user is admin or not.

--- AeroBites/Extensions.cs
+++ AeroBites/Extensions.cs
@@ -10,5 +10,12 @@ namespace AeroBites
         public static int GetId(this ClaimsPrincipal user) {
             return int.Parse(user.FindFirstValue(ClaimTypes.NameIdentifier) ?? "0");
         }
+
+        /// <summary>
+        /// This method returns saying if the user is or not admin
+        /// </summary>
+        public static bool IsAdmin(this ClaimsPrincipal user) {
+            return (user.FindFirstValue("IsAdmin") ?? "").Equals("True");
+        }
     }
 }

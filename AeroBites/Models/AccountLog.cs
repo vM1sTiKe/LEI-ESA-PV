b91a58527b1ed275b1e7bf2dd8e69ddc0c5e10fa
F1nix <74712128+F1nix@users.noreply.github.com>
2025-03-05 15:51:01 +0000

    .

---
+++ AeroBites/Models/AccountLog.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AeroBites.Models
+{
+    public class AccountLog
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public required DateTime signInDate { get; set; }
+    }
+}

253d8ebb552d55f868483d74238cfb3602981017
Bruno Daniel <48827010+vM1sTiKe@users.noreply.github.com>
2025-03-11 17:25:55 +0000

    Migration

--- AeroBites/Migrations/20250311172309_Removed-Unused.Designer.cs
+++ AeroBites/Migrations/20250311172309_Removed-Unused.Designer.cs
@@ -4,6 +4,7 @@ using AeroBites.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AeroBites.Migrations
 {
     [DbContext(typeof(AeroBitesContext))]
-    partial class AeroBitesContextModelSnapshot : ModelSnapshot
+    [Migration("20250311172309_Removed-Unused")]
+    partial class RemovedUnused
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

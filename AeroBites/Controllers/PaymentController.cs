fa9036226fe0c0890299e99a91d97ed0eda53aaf
Pedro Serpe <pserpe01@hotmail.com>
2025-03-14 12:51:48 +0000

    Criação do controller PaymentController
    Criação do controller com a route para a página do checkout.

---
+++ AeroBites/Controllers/PaymentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AeroBites.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}

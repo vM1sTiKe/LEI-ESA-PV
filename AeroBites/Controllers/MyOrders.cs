f18934917b07ca4887b486065aba81e338b09feb
Bruno Daniel <48827010+vM1sTiKe@users.noreply.github.com>
2025-03-17 14:45:25 +0000

    client_history
    Olá sou o francisco

--- AeroBites/Controllers/MyOrders.cs
+++ AeroBites/Controllers/MyOrders.cs
@@ -1,17 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AeroBites.Data;
+using AeroBites.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AeroBites.Controllers
 {
-    public class MyOrders : Controller
+    public class MyOrders(AeroBitesContext context) : Controller
     {
+        private IQueryable<Cart>? Orders => context.Cart.Where(c => c.AccountId == User.GetId());
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult History()
+        /// <summary>
+        /// Obtém a lista de pedidos que já foram entregues, ou seja, no estado "Recieved".
+        /// </summary>
+        /// <returns>
+        /// Retorna uma view com a lista de pedidos ou a mesma view mas com uma lista vazia caso não existam pedidos nessas condições.
+        /// </returns>
+        public async Task<IActionResult> History()
         {
-            return View();
+            if(Orders == null) { return View(new List<Cart>()); }
+
+            var orders = await Orders.Include(c => c.Items).Where(c => c.Status == Enums.OrderStatus.Recieved).OrderByDescending(c => c.Id).ToListAsync();
+
+            return View(orders);
         }
     }
 }

68a9bbb6472f1eb936d68bd0974354911113fafd
Pedro Serpe <202002172@estudantes.ips.pt>
2025-03-25 15:57:28 +0000

    Criação da view, controller e scss de Address
    Foram adicionados os ficheiros referentes ao address:
    - View, Index;
    - Controller, AddressController;
    - Scss, address.
    
    Foi adicionado ainda um icon para redirecionar para a página das moradas.

---
+++ AeroBites/Controllers/AddressController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AeroBites.Controllers
+{
+    public class AddressController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

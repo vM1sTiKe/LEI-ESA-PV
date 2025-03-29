2a04e181d72f8e85759cee987cacff9715318ca4
Bruno Daniel <48827010+vM1sTiKe@users.noreply.github.com>
2025-03-29 18:23:30 +0000

    Tests errors
    "Fixed" problem on test controllers

--- AeroBitesTest/CartControllerTest.cs
+++ AeroBitesTest/CartControllerTest.cs
@@ -29,7 +29,7 @@ namespace AeroBitesTest
             _context = new AeroBitesContext(options);
             _controllerCart = new CartController(_context);
             _controllerCheck = new CheckoutController(_context);
-            _controllerRest = new MyRestaurantController(_context);
+            _controllerRest = new MyRestaurantController(_context, null);
 
             var claims = new List<Claim> { new Claim(ClaimTypes.NameIdentifier, "1") };
             var identity = new ClaimsIdentity(claims, "Cookies");

d2f00d49c02559cc2601945cd574a7c4578272c7
Bruno Daniel <48827010+vM1sTiKe@users.noreply.github.com>
2025-03-29 18:24:02 +0000

    Payments Methods
    Created page to list, add and remove users payment methods.

---
+++ AeroBites/Controllers/PaymentMethods.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AeroBites.Controllers
+{
+    public class PaymentMethods : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Add()
+        {
+            // Código para adicionar novo metodo
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult Remove(int id)
+        {
+            // Codigo para remover método
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}

11cc027f545240923f20f660b2cb09c17465880e
Pedro Serpe <pserpe01@hotmail.com>
2025-04-02 01:33:01 +0100

    Funcionalidade de alteração de morada
    Foi implementada a funcionalidade de o utilizador trocar a morada em que pretende que sejam entregues os seus pedidos.

--- AeroBites/Controllers/RestaurantController.cs
+++ AeroBites/Controllers/RestaurantController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using AeroBites.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace AeroBites.Controllers
 {
@@ -16,6 +17,15 @@ namespace AeroBites.Controllers
         /// A view with a list of valid restaurants.
         /// </returns>
         public IActionResult Index() {
+            var userId = User.GetId();
+
+            var addresses = context.Address.Where(a => a.AccountId == userId).ToList();
+
+            var activeAddressId = addresses.FirstOrDefault(a => a.IsActive)?.Id;
+
+            ViewBag.Addresses = new SelectList(addresses, "Id", "FullAddress", activeAddressId);
+            ViewBag.ActiveAddressId = activeAddressId;
+
             var restaurants = context.Restaurant.Where(restaurant => restaurant.Status == Enums.RestaurantStatus.Valid).ToList();
             return View(restaurants);
         }
@@ -32,6 +42,23 @@ namespace AeroBites.Controllers
             var restaurant = context.Restaurant.Include(r => r.Categories).ThenInclude(c => c.Items).FirstOrDefault(r => r.Id == id);
             ViewBag.Cart = context.Cart.Include(c => c.Items).Where(c => c.AccountId == User.GetId() && c.Status == Enums.OrderStatus.Choosing).FirstOrDefault();
             return View(restaurant);
+        }
+
+
+        [HttpPost]
+        public IActionResult SetActiveAddress(int addressId)
+        {
+            var userId = User.GetId();
+
+            var addresses = context.Address.Where(a => a.AccountId == userId).ToList();
+            foreach (var address in addresses)
+            {
+                address.IsActive = address.Id == addressId;
+            }
+
+            context.SaveChanges();
+
+            return Ok();
         }
     }
 }
